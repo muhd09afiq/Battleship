1d2d29e9c361153d1ce82b849ae1387220b4970d
blazion09 <muhd09afiq@gmail.com>
2024-08-14 13:57:31 +0800

    wip receiveAttack
    end game when all ship sunk

--- src/test/receiveAttack.test.js
+++ src/test/receiveAttack.test.js
@@ -4,17 +4,43 @@ import { Ship } from "../component/ship";
 describe("Receive Attack Test", () => {
   let board;
   let ship;
+  let ship2;
 
   beforeEach(() => {
     ship = new Ship(3);
+    ship2 = new Ship(4);
     board = new Gameboard();
   });
 
-  test("should return hit on occupied coordinate and ship hp to lower", () => {
+  test("should return hit on occupied coordinate", () => {
     board.placeShip(ship, "A", 1);
+    board.placeShip(ship2, "C", 1);
     expect(board.receiveAttack("A1")).toBe("hit");
-    expect(ship.getHit()).toBe(1);
     expect(board.receiveAttack("A2")).toBe("hit");
+    expect(board.receiveAttack("A3")).toBe("hit");
+  });
+
+  test("should return miss on unoccupied coordinate", () => {
+    board.placeShip(ship, "A", 1);
+    expect(board.receiveAttack("B1")).toBe("miss");
+    expect(board.receiveAttack("B2")).toBe("miss");
+    expect(board.receiveAttack("B3")).toBe("miss");
+  });
+
+  test("should increase hit count for ship that was hit", () => {
+    board.placeShip(ship, "A", 1);
+    board.receiveAttack("A1");
+    expect(ship.getHit()).toBe(1);
+    board.receiveAttack("A2");
     expect(ship.getHit()).toBe(2);
+    board.receiveAttack("B2");
+    expect(ship.getHit()).toBe(2);
+  });
+
+  test("should report if all ship has sunk", () => {
+    board.placeShip(ship, "A", 1);
+    board.receiveAttack("A1");
+    board.receiveAttack("A2");
+    expect(board.receiveAttack("A3")).toBe("game end");
   });
 });
