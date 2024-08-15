3dbc2b4a35e3cfd0795de221ec3f68281f48fc7c
blazion09 <muhd09afiq@gmail.com>
2024-08-15 23:36:44 +0800

    wip gridDom

--- src/DOM/grid.js
+++ src/DOM/grid.js
@@ -29,3 +29,33 @@ export function createGridPlayer2() {
     }
   }
 }
+
+export class CreateGrid {
+  constructor(player) {
+    this.player = player;
+  }
+
+  createContainer() {
+    const container = document.createElement("div");
+    container.id = `${this.player}-grid`;
+    container.classList.add("grid-container");
+    const wrapper = document.querySelector(".wrapper");
+    wrapper.appendChild(container);
+  }
+
+  createGridPlayer() {
+    const xLabels = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
+    const yLabels = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
+    const gridContainer = document.getElementById(`${this.player}-grid`);
+
+    for (let row = 0; row < 10; row++) {
+      for (let col = 0; col < 10; col++) {
+        const cell = document.createElement("div");
+        cell.classList.add("grid-square");
+        cell.id = cell.id = `${yLabels[row]}${xLabels[col]}`;
+
+        gridContainer.appendChild(cell);
+      }
+    }
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import "./css/reset.css";
 import "./css/style.css";
-import { createGridPlayer1, createGridPlayer2 } from "./DOM/grid";
+import { createGridPlayer1, createGridPlayer2, CreateGrid } from "./DOM/grid";
 
-createGridPlayer1();
-createGridPlayer2();
+// createGridPlayer1();
+// createGridPlayer2();
+
+const player1 = new CreateGrid("player1");
+const player2 = new CreateGrid("player2");
+
+document.addEventListener("DOMContentLoaded", function () {
+  player1.createContainer();
+  player1.createGridPlayer();
+  player2.createContainer();
+  player2.createGridPlayer();
+});
